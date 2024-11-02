39a91cc9abe9a6051983bbbf8598398201135245
kidusshoa <captainkevin2008@gmail.com>
2024-11-03 02:39:31 +0300

    setup keys

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { validate } from "../middlewares/validate.js";
 import { RestaurantSchema, type Restaurant } from "../schemas/restaurant.js";
+import { initializedRedisClient } from "../utils/client.js";
 const router = express.Router();
 
 router.post("/", validate(RestaurantSchema), async (req, res) => {
   const data = req.body as Restaurant;
+  const client = await initializedRedisClient();
   res.send("Hello world!");
 });
 export default router;
