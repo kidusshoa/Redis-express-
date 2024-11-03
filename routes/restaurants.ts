405c0ada197c6681275e87a533be2b1f7328febd
kidusshoa <captainkevin2008@gmail.com>
2024-11-03 03:06:26 +0300

    hash

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -2,11 +2,25 @@ import express from "express";
 import { validate } from "../middlewares/validate.js";
 import { RestaurantSchema, type Restaurant } from "../schemas/restaurant.js";
 import { initializedRedisClient } from "../utils/client.js";
+import { nanoid } from "nanoid";
+import { restaurantKeyById } from "../utils/keys.js";
+import { successResponse } from "../utils/responses.js";
 const router = express.Router();
 
-router.post("/", validate(RestaurantSchema), async (req, res) => {
+router.post("/", validate(RestaurantSchema), async (req, res, next) => {
   const data = req.body as Restaurant;
-  const client = await initializedRedisClient();
-  res.send("Hello world!");
+  try {
+    const client = await initializedRedisClient();
+    const id = nanoid();
+    const restaurantKey = restaurantKeyById(id);
+    const hashData = { id, name: data.name, location: data.location };
+    const addResult = await client.hSet(restaurantKey, hashData);
+    console.log(`Added ${addResult} fields`);
+
+    successResponse(res, hashData, "added new restaurant");
+    return;
+  } catch (error) {
+    next(error);
+  }
 });
 export default router;
