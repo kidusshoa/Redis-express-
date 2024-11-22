983801c16cee2e99efe3f48f5b986d59bfb23a56
kidusshoa <captainkevin2008@gmail.com>
2024-11-22 19:34:28 +0300

    get reviews

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -61,6 +61,29 @@ router.post(
 );
 
 router.get(
+  "/:restaurantId/reviews",
+  checkRestaurantExists,
+  async (req: Request<{ restaurantId: string }>, res, next) => {
+    const { restaurantId } = req.params;
+    const { page = 1, limit = 10 } = req.query;
+    const start = (Number(page) - 1) * Number(limit);
+    const end = start * Number(limit) - 1;
+
+    try {
+      const client = await initializedRedisClient();
+      const reviewKey = reviewKeyById(restaurantId);
+      const reviewIds = await client.lRange(reviewKey, start, end);
+      const reviews = await Promise.all(
+        reviewIds.map((id) => client.hGetAll(reviewDetailsKeyById(id)))
+      );
+      successResponse(res, reviews);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get(
   "/:restaurantId",
   checkRestaurantExists,
   async (req: Request<{ restaurantId: string }>, res, next) => {
