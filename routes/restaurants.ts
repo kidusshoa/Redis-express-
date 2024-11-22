8981e0b03992011875a3466970ab44cf0732a663
kidusshoa <captainkevin2008@gmail.com>
2024-11-22 18:02:13 +0300

    reviews

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -3,9 +3,15 @@ import { validate } from "../middlewares/validate.js";
 import { RestaurantSchema, type Restaurant } from "../schemas/restaurant.js";
 import { initializedRedisClient } from "../utils/client.js";
 import { nanoid } from "nanoid";
-import { restaurantKeyById } from "../utils/keys.js";
+import {
+  restaurantKeyById,
+  reviewDetailsKeyById,
+  reviewKeyById,
+} from "../utils/keys.js";
 import { successResponse } from "../utils/responses.js";
 import { checkRestaurantExists } from "../middlewares/checkRestaurantExists.js";
+import { ReviewSchema, type Review } from "../schemas/review.js";
+import { timeStamp } from "console";
 const router = express.Router();
 
 router.post("/", validate(RestaurantSchema), async (req, res, next) => {
@@ -24,6 +30,35 @@ router.post("/", validate(RestaurantSchema), async (req, res, next) => {
     next(error);
   }
 });
+
+router.post(
+  "/:restaurantId/reviews",
+  checkRestaurantExists,
+  validate(ReviewSchema),
+  async (req: Request<{ restaurantId: string }>, res, next) => {
+    const { restaurantId } = req.params;
+    const data = req.body as Review;
+    try {
+      const client = await initializedRedisClient();
+      const reviewId = nanoid();
+      const reviewKey = reviewKeyById(restaurantId);
+      const reviewDetailsKey = reviewDetailsKeyById(reviewId);
+      const reviewData = {
+        id: reviewId,
+        ...data,
+        timeStamp: Date.now(),
+        restaurantId,
+      };
+      await Promise.all([
+        client.lPush(reviewKey, reviewId),
+        client.hSet(reviewDetailsKey, reviewData),
+      ]);
+      successResponse(res, reviewData, "Review added");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 router.get(
   "/:restaurantId",
