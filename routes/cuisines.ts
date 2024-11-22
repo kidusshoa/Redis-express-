25b7c0d89514fae5919a02e791878ba42d967cb3
kidusshoa <captainkevin2008@gmail.com>
2024-11-22 21:07:39 +0300

    resturants based on cuisines

--- routes/cuisines.ts
+++ routes/cuisines.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { initializedRedisClient } from "../utils/client.js";
-import { cuisineKey, cuisinesKey } from "../utils/keys.js";
+import { cuisineKey, cuisinesKey, restaurantKeyById } from "../utils/keys.js";
 import { successResponse } from "../utils/responses.js";
 const router = express.Router();
 
@@ -9,7 +9,23 @@ router.get("/", async (req, res, next) => {
     const client = await initializedRedisClient();
     const cuisines = await client.sMembers(cuisinesKey);
     successResponse(res, cuisines);
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:cuisine", async (req, resolve, next) => {
+  const { cuisine } = req.params;
+  try {
+    const client = await initializedRedisClient();
+    const restaurantIds = await client.sMembers(cuisineKey(cuisine));
+    const restaurants = await Promise.all(
+      restaurantIds.map((id) => client.hGet(restaurantKeyById(id), "name"))
+    );
+    successResponse(resolve, restaurants);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
