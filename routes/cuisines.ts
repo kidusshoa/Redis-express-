9a08b195df0f9a561ab8056dd1f536d10f6377e2
kidusshoa <captainkevin2008@gmail.com>
2024-11-22 20:56:09 +0300

    get cuisines

--- routes/cuisines.ts
+++ routes/cuisines.ts
@@ -1,5 +1,15 @@
 import express from "express";
-
+import { initializedRedisClient } from "../utils/client.js";
+import { cuisineKey, cuisinesKey } from "../utils/keys.js";
+import { successResponse } from "../utils/responses.js";
 const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const client = await initializedRedisClient();
+    const cuisines = await client.sMembers(cuisinesKey);
+    successResponse(res, cuisines);
+  } catch (error) {}
+});
 
 export default router;
